47373f4c4f312e30582d23097953b40e4420acdb
Enes Dertli <Enesdertli05@hotmail.com>
2022-12-08 15:14:14 +0300

    added dia and enemy

---
+++ Assets/Script/EnemyController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+
+    Rigidbody2D rigidbody;
+    float velocity = 5f;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    
+    void Update()
+    {
+        rigidbody.velocity = new Vector2(velocity, 0);
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        transform.localScale = new Vector2(-(Mathf.Sign(rigidbody.velocity.x)), 1);
+        velocity = -velocity;
+    }
+}
--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -6,9 +6,11 @@ public class PlayerMovement : MonoBehaviour
 {
     Rigidbody2D rigidbody;
     Vector3 velocity;
-    float JumpForce = 5f;
-    float speed = 3f;
     Animator playerAnimator;
+    float JumpForce = 7f;
+    float speed = 3f;
+    float points = 0;
+
     void Start()
     {
         rigidbody = GetComponent<Rigidbody2D>();
@@ -18,26 +20,41 @@ public class PlayerMovement : MonoBehaviour
  
     void Update()
     {
+        MovementControl();
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Diamond") {
+            Destroy(GameObject.FindWithTag("Diamond"));
+            points += 5;
+        }
+    }
+
+    private void MovementControl()
+    {
         velocity = new Vector3(Input.GetAxis("Horizontal"), 0);
         transform.position += velocity * Time.deltaTime * speed;
-        if (Input.GetButtonDown("Jump") && rigidbody.velocity.y == 0) {
-            rigidbody.AddForce(Vector3.up*JumpForce, ForceMode2D.Impulse);
+        if (Input.GetButtonDown("Jump") && rigidbody.velocity.y == 0)
+        {
+            rigidbody.AddForce(Vector3.up * JumpForce, ForceMode2D.Impulse);
             playerAnimator.SetBool("isJumping", true);
         }
-        if (playerAnimator.GetBool("isJumping") && rigidbody.velocity.y == 0) {
+        if (playerAnimator.GetBool("isJumping") && rigidbody.velocity.y == 0)
+        {
             playerAnimator.SetBool("isJumping", false);
         }
-        if (Input.GetAxisRaw("Horizontal") == 1) {
+        if (Input.GetAxisRaw("Horizontal") == 1)
+        {
             transform.localScale = new Vector3(1, 1);
         }
-        if (Input.GetAxisRaw("Horizontal") == -1) {
+        if (Input.GetAxisRaw("Horizontal") == -1)
+        {
             transform.localScale = new Vector3(-1, 1);
         }
 
-        bool playerHasHorizontalSpeed = Mathf.Abs(velocity.x)  > Mathf.Epsilon;
+        bool playerHasHorizontalSpeed = Mathf.Abs(velocity.x) > Mathf.Epsilon;
         playerAnimator.SetBool("isRunning", playerHasHorizontalSpeed);
-
-
-
     }
 }
